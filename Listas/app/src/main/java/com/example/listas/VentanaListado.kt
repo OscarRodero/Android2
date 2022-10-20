c1e1e2896439cae28f7d8351c844e95eaefb9212
Óscar_Rodero <oscarrodero@hotmail.com>
2022-10-20 11:02:20 +0200

    Almacenaje ya funciona

--- Listas/app/src/main/java/com/example/listas/VentanaListado.kt
+++ Listas/app/src/main/java/com/example/listas/VentanaListado.kt
@@ -7,5 +7,6 @@ class VentanaListado : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ventana_listado)
+        val adaptador = ArrayAdapter(this,R.layout.item_lista, R.id.txtItem, Almacen)
     }
 }
