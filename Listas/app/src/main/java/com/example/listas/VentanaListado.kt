ff3d0a521cb08755e9e8633de271eff38b997b6c
Óscar_Rodero <oscarrodero@hotmail.com>
2022-10-20 11:13:01 +0200

    Adaptador creado en VentanaListado

--- Listas/app/src/main/java/com/example/listas/VentanaListado.kt
+++ Listas/app/src/main/java/com/example/listas/VentanaListado.kt
@@ -1,12 +1,14 @@
 package com.example.listas
 
+import Model.Almacen
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 
 class VentanaListado : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ventana_listado)
-        val adaptador = ArrayAdapter(this,R.layout.item_lista, R.id.txtItem, Almacen)
+        val adaptador = ArrayAdapter(this,R.layout.item_layout, R.id.txtRegistro, Almacen.Almacenaje)
     }
 }
