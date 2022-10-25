a8589ca16f0a9d29361c542dc66299db4b5d1c65
Óscar_Rodero <oscarrodero@hotmail.com>
2022-10-25 13:05:36 +0200

    Añadidos cambios a la ventana registro.
    Añadido adicional al manifest la línea :
    <uses-permission android:name="android.permission.INTERNET"/>

--- RegistroRecycle/app/src/main/java/com/example/registrorecycle/Registro.kt
+++ RegistroRecycle/app/src/main/java/com/example/registrorecycle/Registro.kt
@@ -1,11 +1,27 @@
 package com.example.registrorecycle
 
+import Modelo.Almacen
+import Modelo.Apunte
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.registrorecycle.databinding.ActivityRegistroBinding
 
 class Registro : AppCompatActivity() {
+    lateinit var binding: ActivityRegistroBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registro)
+        //setContentView(R.layout.activity_registro)
+        binding = ActivityRegistroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnConfirmarRegistro.setOnClickListener(){
+            if(binding.etxtNombre.text.toString()!="" && binding.etxtApellidos.text.toString()!=""){
+                var n = Apunte(binding.etxtNombre.text.toString(), binding.etxtApellidos.text.toString())
+                Almacen.addRegistro(n)
+                finish()
+            }
+        }
+        binding.btnCancelar.setOnClickListener(){
+            finish()
+        }
     }
 }
