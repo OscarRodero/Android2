084eb26601e41b855f1b91fa8c62e7cdca0f797b
Óscar_Rodero <oscarrodero@hotmail.com>
2022-10-25 13:19:09 +0200

    Insertada función result launcher

--- RegistroRecycle/app/src/main/java/com/example/registrorecycle/MainActivity.kt
+++ RegistroRecycle/app/src/main/java/com/example/registrorecycle/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.example.registrorecycle
 
+import Modelo.Almacen
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.registrorecycle.databinding.ActivityMainBinding
+import com.example.registrorecycle.databinding.ActivityRegistroBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+
+    var resultLauncher = registerForActivityResult(ActivityRegistroBinding.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            // There are no request codes
+            val data: Intent? = result.data
+            // Get data from Intent
+            val re: Registro = data!!.getSerializableExtra("objRegistro") as Registro
+            Almacen.addRegistro(re)
+
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
