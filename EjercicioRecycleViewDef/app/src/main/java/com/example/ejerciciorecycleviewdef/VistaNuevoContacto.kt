4ca7056762e1e78c4b9b278d736c596f6405713e
Óscar Rodero <oscarrodero@hotmail.com>
2022-10-27 20:53:22 +0200

    Intento de creación de contactos con foto personalizada.

---
+++ EjercicioRecycleViewDef/app/src/main/java/com/example/ejerciciorecycleviewdef/VistaNuevoContacto.kt
@@ -0,0 +1,11 @@
+package com.example.ejerciciorecycleviewdef
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VistaNuevoContacto : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vista_nuevo_contacto)
+    }
+}
