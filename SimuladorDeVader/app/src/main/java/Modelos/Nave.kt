670e093bc9a7e135850cdb523d13259fe6e0cc8d
Óscar Rodero <oscarrodero@hotmail.com>
2022-11-07 17:28:22 +0100

    Creadas las clases Piloto y Nave

---
+++ SimuladorDeVader/app/src/main/java/Modelos/Nave.kt
@@ -0,0 +1,3 @@
+package Modelos
+
+data class Nave(var license:String, var type:String, var canCarryCargo:Boolean, var image:String?)
---
+++ SimuladorDeVader/app/src/main/java/Modelos/Piloto.kt
@@ -0,0 +1,3 @@
+package Modelos
+
+data class Piloto(var name:String, var age:Int, var exp:Int, var password:String)
