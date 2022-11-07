6214826678902b6da9fd8ced9c85087cb929cd96
Óscar Rodero <oscarrodero@hotmail.com>
2022-11-07 17:15:00 +0100

    El menú funciona, pero hay que crear las funciones que habrá de utilizar

--- SimuladorDeVader/app/src/main/java/com/example/simuladordevader/MainActivity.kt
+++ SimuladorDeVader/app/src/main/java/com/example/simuladordevader/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.simuladordevader
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.example.simuladordevader.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +13,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?):Boolean{
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem):Boolean{
+        when(item.itemId){ //Cada una es una de las opciones, cambiar al gusto.
+            R.id.menuAltaPilotos -> Toast.makeText(this, "Guardar",Toast.LENGTH_LONG).show()
+            R.id.menuAltaNaves -> Toast.makeText(this, "Perfil",Toast.LENGTH_LONG).show()
+            R.id.menuCrearMisiones -> Toast.makeText(this, "Salir",Toast.LENGTH_LONG).show()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
