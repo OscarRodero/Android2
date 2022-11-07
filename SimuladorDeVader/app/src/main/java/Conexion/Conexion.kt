c6dbf096298c79ff632dde8103f21e425b74bf85
Óscar Rodero <oscarrodero@hotmail.com>
2022-11-07 19:57:31 +0100

    Creada la conexión y la administración de SQLite
    Aparte se añadieron un par de métodos para poder insertar y eliminar tanto naves como pilotos en la base de datos, en sus respectivas tablas.

---
+++ SimuladorDeVader/app/src/main/java/Conexion/Conexion.kt
@@ -0,0 +1,51 @@
+package Conexion
+
+import Modelos.Nave
+import Modelos.Piloto
+import android.content.ContentValues
+import androidx.appcompat.app.AppCompatActivity
+
+object Conexion {
+    var nombreDB = "administracion.db3"
+
+    fun cambiarDB(nombreDB:String){
+        this.nombreDB=nombreDB
+    }
+
+    fun addPiloto(contexto: AppCompatActivity, p: Piloto){
+        val admin = AdminSQLiteConexion(contexto, nombreDB, null, 1)
+        val db = admin.writableDatabase
+        val registro = ContentValues()
+        registro.put("name", p.name)
+        registro.put("age", p.age)
+        registro.put("exp", p.exp)
+        registro.put("password", p.password)
+        db.insert("pilotos", null, registro)
+        db.close()
+    }
+    fun delPiloto(contexto: AppCompatActivity, name:String):Int{
+        val admin = AdminSQLiteConexion(contexto, nombreDB, null, 1)
+        val db = admin.writableDatabase
+        val cant = db.delete("pilotos", "name='${name}'", null)
+        db.close()
+        return cant
+    }
+    fun addNave(contexto: AppCompatActivity, n: Nave){
+        val admin = AdminSQLiteConexion(contexto, nombreDB, null, 1)
+        val db = admin.writableDatabase
+        val registro = ContentValues()
+        registro.put("license", n.license)
+        registro.put("type", n.type)
+        registro.put("canCarryCargo", n.canCarryCargo)
+        registro.put("image", n.image)
+        db.insert("naves", null, registro)
+        db.close()
+    }
+    fun delNave(contexto: AppCompatActivity, license:String):Int{
+        val admin = AdminSQLiteConexion(contexto, nombreDB, null, 1)
+        val db = admin.writableDatabase
+        val cant = db.delete("naves", "license='${license}'", null)
+        db.close()
+        return cant
+    }
+}
