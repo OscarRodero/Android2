f07222e1c9410e2c24bdd0928f5631bad1ef0eb9
Óscar Rodero <oscarrodero@hotmail.com>
2022-11-08 20:47:23 +0100

    AAñadido página login, añadido logo y añadido resultLauncher.

--- SimuladorDeVader/app/src/main/java/com/example/simuladordevader/MainActivity.kt
+++ SimuladorDeVader/app/src/main/java/com/example/simuladordevader/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.simuladordevader
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,17 +8,28 @@ import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.simuladordevader.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     lateinit var inte:Intent
+    var resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
+        if(result.resultCode == Activity.RESULT_OK){
+            val data:Intent? = result.data
+            val LogName = data!!.getStringExtra("QuienLoggea")
+            val LogPassword = data!!.getStringExtra("Contraseña")
 
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        binding.Inicio.setOnClickListener(){
+            inte = Intent(this, AltaNaves::class.java)
+            startActivity(inte)
+        }
 
     }
 
