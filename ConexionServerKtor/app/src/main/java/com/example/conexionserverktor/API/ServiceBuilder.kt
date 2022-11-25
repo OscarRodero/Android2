816f862d487f1620f050f07fc41d9cb0d355d5d0
Óscar Rodero <oscarrodero@hotmail.com>
2022-11-25 19:16:19 +0100

    Proyecto dbktor creado.

--- ConexionServerKtor/app/src/main/java/com/example/conexionserverktor/API/ServiceBuilder.kt
+++ ConexionServerKtor/app/src/main/java/com/example/conexionserverktor/API/ServiceBuilder.kt
@@ -1,5 +1,11 @@
 package com.example.conexionserverktor.API
 
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object ServiceBuilder {
     val client = OkHttpClient.Builder().build()
     val url = "http://192.168.2.250:8080"
